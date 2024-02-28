d8e1941f8cafa8283c086215e2b9bd2a3ad86c5b
yangcan1 <yangca@oregonstate.edu>
2024-02-27 16:41:40 -0800

    changes

--- Assignments/Assignment-4/main.cpp
+++ Assignments/Assignment-4/main.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include "record_class.h"
 #include <algorithm>
+#include <cstdio>
 
 using namespace std;
 #define buffer_size 22
@@ -18,16 +19,9 @@ int run = 0;
 int flag = 0;
 
 
-void PrintSorted(){
-
-    //Store in EmpSorted.csv
-    return;
-}
-
 bool compareByID(const Records& a, const Records& b) {
     return a.emp_record.eid < b.emp_record.eid;
 }
-
 //PASS 1
 //Sorting the buffers in main_memory and storing the sorted records into a temporary file (Runs) 
 void Sort_Buffer(Records buffers[buffer_size]){
@@ -44,9 +38,6 @@ void Sort_Buffer(Records buffers[buffer_size]){
         return;
     }
     sort(buffers, buffers + buffer_size, compareByID);
-    // for (int i = 0; i < buffer_size; i++) {
-    //     cout << "i: eID: " << i << ":" << buffers[i].emp_record.eid << "\n";
-    // }
     string filename = "runs" + to_string(run) + ".txt";
     fstream runs;
     runs.open(filename, ios::in| ios::out | ios::app);
@@ -62,9 +53,64 @@ void Sort_Buffer(Records buffers[buffer_size]){
 //PASS 2
 //Use main memory to Merge the Runs 
 //which are already sorted in 'runs' of the relation Emp.csv 
-void Merge_Runs(){
-    // 
-    return;
+void Merge_Runs() {
+    vector<ifstream> runFiles(run);
+    for (int i = 0; i < run; ++i) {
+        string filename = "runs" + to_string(i) + ".txt";
+        runFiles[i].open(filename);
+    }
+
+    // Open output file
+    ofstream SortOut("EmpSorted.csv");
+
+    // Initialize variables to store current record from each run file
+    vector<Records> currentRecords(run);
+    for (int i = 0; i < run; ++i) {
+        string line;
+        if (getline(runFiles[i], line)) {
+            currentRecords[i] = Grab_Emp_Record(line);
+        }
+    }
+
+    // Merge runs
+    while (1) {
+        // Find the smallest record
+        Records smallestRecord;
+        int smallestRecordIndex = -1;
+        for (int i = 0; i < run; ++i) {
+            if (!(currentRecords[i].no_values == -1)) {
+                if (smallestRecordIndex == -1 || currentRecords[i].emp_record.eid < smallestRecord.emp_record.eid) {
+                    smallestRecord = currentRecords[i];
+                    smallestRecordIndex = i;
+                }
+            }
+        }
+
+        // If all runs are empty, break the loop
+        if (smallestRecordIndex == -1) {
+            break;
+        }
+
+        // Write smallest record to output file
+        SortOut << smallestRecord.emp_record.eid << "," << smallestRecord.emp_record.ename << "," << smallestRecord.emp_record.age << "," << smallestRecord.emp_record.salary << "\n";
+
+        // Read the next record from the run file from which smallest record was selected
+        string line;
+        if (getline(runFiles[smallestRecordIndex], line)) {
+            currentRecords[smallestRecordIndex] = Grab_Emp_Record(line);
+        } else {
+            // If the run file is empty, mark the current record as empty
+            currentRecords[smallestRecordIndex].no_values = -1;
+        }
+    }
+
+    // Close all run files
+    for (int i = 0; i < run; ++i) {
+        runFiles[i].close();
+    }
+
+    // Close output file
+    SortOut.close();
 }
 
 
@@ -76,6 +122,12 @@ int main() {
     int count = 0;
 
     empin.open("Emp.csv", ios::in);
+
+    for (int i = 0; i < 19; i++) {
+        string fname = "runs" + to_string(i) + ".txt";
+        remove(fname.c_str());
+    }
+
     while (getline(empin, line))
     {
         Records records = Grab_Emp_Record(line);
@@ -90,9 +142,17 @@ int main() {
     empin.close();
 
     //Creating the EmpSorted.csv file where we will store our sorted results
-    fstream SortOut;
-    SortOut.open("EmpSorted.csv", ios::out | ios::app);
-    SortOut.close();
+    remove("EmpSorted.csv");
+    Merge_Runs();
+    // fstream SortOut;
+    // SortOut.open("EmpSorted.csv", ios::out | ios::app);
+    // SortOut.close();
+
+
+    for (int i = 0; i < 19; i++) {
+        string fname = "runs" + to_string(i) + ".txt";
+        remove(fname.c_str());
+    }
 
     return 0;
 }
